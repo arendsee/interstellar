a0e2564b0c164ae805051202c85d995e08c5a9a0
Zebulun Arendsee <arendsee@iastate.edu>
2015-05-05 12:02:00 -0500

    refactored sim.R

--- sim.R
+++ sim.R
@@ -1,3 +1,11 @@
 #!/usr/bin/env Rscript
 source('functions.R')
-run.death.simulations(k=1000)
+
+## Uncomment to run simulation with varying population size
+# pop.sim(k=1250)
+
+## Uncomment to run simulation with varying maximum lifespans
+# age.sim(k=1250)
+
+## Uncomment to run simulation with varying fertility rates
+# fertility.sim(k=1)
